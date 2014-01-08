8c28fdb715206ffa9c05ea6adedb5a1a441b2523
Daylen Yang <services@daylenyang.com>
2014-01-07 17:41:09 -0800

    Initial Commit

---
+++ Stockfish/SFMDocument.h
@@ -0,0 +1,13 @@
+//
+//  SFMDocument.h
+//  Stockfish
+//
+//  Created by Daylen Yang on 1/7/14.
+//  Copyright (c) 2014 Daylen Yang. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SFMDocument : NSDocument
+
+@end
---
+++ Stockfish/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Stockfish
+//
+//  Created by Daylen Yang on 1/7/14.
+//  Copyright (c) 2014 Daylen Yang. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
